6cb525c0b7d123d4fe1dac4492a7588922b502e3
kamui00002 <kamui00002@yahoo.co.jp>
2025-10-17 23:29:13 +0900

    IOS化

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,37 +1,37 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-    appId: 'com.parkpedia.app',
-    appName: 'ParkPedia',
-    webDir: 'dist',
-    server: {
-        androidScheme: 'https'
+  appId: 'com.parkpedia.app',
+  appName: 'ParkPedia',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  },
+  ios: {
+    scheme: 'ParkPedia',
+    contentInset: 'automatic'
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      launchAutoHide: true,
+      backgroundColor: "#16a34a",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: false,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#ffffff",
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: "launch_screen",
+      useDialog: true,
     },
-    ios: {
-        scheme: 'ParkPedia',
-        contentInset: 'automatic'
-    },
-    plugins: {
-        SplashScreen: {
-            launchShowDuration: 2000,
-            launchAutoHide: true,
-            backgroundColor: "#16a34a",
-            androidSplashResourceName: "splash",
-            androidScaleType: "CENTER_CROP",
-            showSpinner: false,
-            androidSpinnerStyle: "large",
-            iosSpinnerStyle: "small",
-            spinnerColor: "#ffffff",
-            splashFullScreen: true,
-            splashImmersive: true,
-            layoutName: "launch_screen",
-            useDialog: true,
-        },
-        StatusBar: {
-            style: 'dark',
-            backgroundColor: '#16a34a'
-        }
+    StatusBar: {
+      style: 'dark',
+      backgroundColor: '#16a34a'
     }
+  }
 };
 
 export default config;
